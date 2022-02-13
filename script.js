43a7f06ceb9c67713731cbba4ef779e153549927
RAHUL RUDRA <rahulrudra245@gmail.com>
2022-02-13 15:58:01 +0600

    fddirst commit

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
-
-
+function getPin(){
+    const pin=Math.round(Math.random()*1000);
+    const pinString=pin+"";
+    if(pinString.length==4){
+        return pin
+    }
+    else{
+        return getPin();
+    }
+}
 function generatePin(){
-  
+  const pin=getPin();
+  document.getElementById('display-pin').value=pin;
 
-    
+
+
 }
